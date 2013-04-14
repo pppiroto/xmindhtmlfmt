43f395807bbfee4f6217def8d45812e9fecf71d3
pppiroto <pppiroto@gmail.com>
2013-04-14 23:28:00 +0900

    FreeStyleWiki書式に変換テスト版完了

--- xmindhtmlfmt/main.py
+++ xmindhtmlfmt/main.py
@@ -1,8 +1,47 @@
-'''
-Created on 2013/04/13
+# -*- coding:utf-8 -*`-
+import os
+import glob
+import codecs
+from bs4 import BeautifulSoup
 
-@author: piroto
-'''
+def freeStyleWiki(text, indent):
+    prefix = ""
+    if indent == 1:
+        prefix = "!!!"
+    elif indent == 2:
+        prefix = "!!"
+    elif indent == 3:
+        prefix = "!"
+    elif indent == 4:
+        prefix = "::"
+    elif indent == 5:
+        prefix = "*"
+    elif indent == 6:
+        prefix = "**"
+    
+    return '%s%s\r\n' % (prefix, text)
 
+def parse(out, node, indent):
+    
+    try:
+        out.write(freeStyleWiki(node['text'], indent))
+    except KeyError:
+        return
+
+    indent = indent + 1
+    for child in node.contents:
+        parse(out, child, indent)
+
+def meke_formatted_text(path):
+    mms = glob.glob(os.path.join(path, "*.mm"))
+    for mm in mms:
+        out = codecs.open(mm + ".out.txt", 'w', 'utf-8')
+        soup = BeautifulSoup(open(mm))
+        parse(out,soup.map.contents[0],1)
+        out.close()
+        
 if __name__ == '__main__':
-    pass
+    target = 'C:\Users\piroto\Dropbox\Private\Maindmap\FreeMindFormat'
+    meke_formatted_text(target)
+    print "finish"
+    
